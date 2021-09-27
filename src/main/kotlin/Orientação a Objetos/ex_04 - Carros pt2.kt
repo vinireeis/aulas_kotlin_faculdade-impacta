5392c9dfb2d8624aef6dd7a4cf87b2eaa4e68a8c
Vinicius Dos Reis Oliveira <v_reis@outlook.com.br>
2021-09-27 20:43:06 -0300

    continuação Orientação a Objetos Kotlin

---
+++ src/main/kotlin/Orientação a Objetos/ex_04 - Carros pt2.kt
@@ -0,0 +1,30 @@
+class Carro(modelo: String, ano: Int, velocidade: Int){
+    private var modelo: String
+    private var ano = 1900
+    private var velocidade: Int
+
+    init {
+        this.modelo =  modelo
+        this.ano = ano
+        this.velocidade = velocidade
+    }
+
+    fun getModelo():String {
+        return modelo
+}
+}
+
+fun main(){
+    var c0 = Carro("Peugeot", 2012, 200)
+    var c1 = Carro("HB20", 2020, 200)
+    var c2 = Carro(modelo = "Fusca", ano = 1950, velocidade = 250)
+    println("Ano do carro 1: ${c1.ano} \nano do carro 2: ${c2.ano}")
+
+    var carros = arrayListOf(c0, c1, c2)
+
+    for (x in carros){
+        if (x.ano < 2000){
+            println("Você precisa vender o ${x.modelo}")
+        }
+    }
+}
